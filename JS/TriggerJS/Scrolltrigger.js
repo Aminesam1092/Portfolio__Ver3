97a96b8eef6f0e9046c3459f188442cdf6a9dffc
Aminesam1092 <doublaliamin13@gmail.com>
2022-04-21 01:44:04 +0900

    First commit

---
+++ JS/TriggerJS/Scrolltrigger.js
@@ -0,0 +1,92 @@
+gsap.to(".Selfintroduction__HeaddingText",{
+    scrollTrigger: {
+        trigger: ".Selfintroduction__HeaddingText",
+        start: "top center",
+        end: '+=1500',
+        scrub: true,
+        pin: true,
+    },
+    opacity: 1,
+});
+
+gsap.to(".Workintroduction__Headdingtext",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__Headdingtext",
+        start: "top center",
+        end: '+=1500',
+        scrub: true,
+        pin: true,
+    },
+    opacity: 1,
+});
+
+gsap.to(".Workintroduction__SecondHeaddingtext",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__SecondHeaddingtext",
+        start: "top center",
+        end: '+=1500',
+        scrub: true,
+        pin: true,
+    },
+    opacity: 0,
+});
+
+gsap.to(".Selfintroduction__Myinfo",{
+    scrollTrigger: {
+        trigger: ".Selfintroduction__Myinfo",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 2,
+    opacity: 1,
+});
+
+gsap.to(".Skillintroduction",{
+    scrollTrigger: {
+        trigger: ".Skillintroduction",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 4,
+    opacity: 1,
+});
+
+gsap.to(".JS--Animation01",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__Website",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 2,
+    opacity: 1,
+});
+
+gsap.to(".JS--Animation02",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__Website",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 3,
+    opacity: 1,
+});
+
+gsap.to(".JS--Animation03",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__Website",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 4,
+    opacity: 1,
+});
+
+gsap.to(".JS--Animation04",{
+    scrollTrigger: {
+        trigger: ".Workintroduction__Website",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 5,
+    opacity: 1,
+});
