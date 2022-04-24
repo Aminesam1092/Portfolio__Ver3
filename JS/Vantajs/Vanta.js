f31f7b9572def3a5f653514134c5aaa82b4763c1
Aminesam1092 <doublaliamin13@gmail.com>
2022-04-24 16:45:13 +0900

    Recommit

--- JS/Vantajs/Vanta.js
+++ JS/Vantajs/Vanta.js
@@ -1,12 +1,15 @@
-VANTA.BIRDS({
-  el: "#JS-BackgroundWave",
-  mouseControls: true,
-  touchControls: true,
+VANTA.NET({
+  el: "#JS-Background",
+  mouseControls: false,
+  touchControls: false,
   gyroControls: false,
   minHeight: 200.00,
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  color: 0x0,
-  backgroundColor: 0xcfcfcf
+  color: 0xd2d2d2,
+  backgroundColor: 0xffffff,
+  points: 7.00,
+  maxDistance: 35.00,
+  spacing: 20.00
 })
