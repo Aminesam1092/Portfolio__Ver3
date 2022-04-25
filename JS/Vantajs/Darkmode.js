2ea89506c71581a5e904cd5b51986b6838eb43fe
Aminesam1092 <doublaliamin13@gmail.com>
2022-04-26 02:13:12 +0900

    Recommit

---
+++ JS/Vantajs/Darkmode.js
@@ -0,0 +1,9 @@
+const VantaDark = function () {
+    document.querySelector('.JS-Background').style.display = "none";
+    document.querySelector('.JS-BackgroundDark').style.display = "block";
+};
+
+const VantaLight = function () {
+    document.querySelector('.JS-Background').style.display = "none";
+    document.querySelector('.JS-BackgroundDark').style.display = "block";
+};
--- JS/Vantajs/Vanta.js
+++ JS/Vantajs/Vanta.js
@@ -12,4 +12,17 @@ VANTA.NET({
   points: 7.00,
   maxDistance: 35.00,
   spacing: 20.00
+});
+
+VANTA.NET({
+  el: ".JS-BackgroundDark",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff7900,
+  backgroundColor: 0x282828
 })
