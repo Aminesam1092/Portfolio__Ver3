cac5ebcb35218c40d1847da17c0b1b2f013292b1
Aminesam1092 <doublaliamin13@gmail.com>
2022-04-27 00:24:21 +0900

    Recommit

--- JS/SwiperJS/Slider.js
+++ JS/SwiperJS/Slider.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper',{
+const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     effect: "fade",
     loop: true,
--- JS/Vantajs/Darkmode.js
+++ JS/Vantajs/Darkmode.js
@@ -1,6 +1,6 @@
 const VantaDark = function () {
-    //document.querySelector('#JS-Background').style.display = "none";
-    //document.querySelector('#JS-BackgroundDark').style.display = "block";
+    document.querySelector('#JS-Background').style.display = "none";
+    document.querySelector('#JS-BackgroundDark').style.display = "block";
 };
 
 const VantaLight = function () {
