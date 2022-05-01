094cb94abfb211074134ffe5fdc32306e16d678d
Aminesam1092 <doublaliamin13@gmail.com>
2022-05-01 14:02:08 +0900

    Recommit

--- JS/Vantajs/Darkmode.js
+++ JS/Vantajs/Darkmode.js
@@ -8,18 +8,42 @@ const VantaLight = function () {
     document.querySelector('#JS-BackgroundDark').style.display = "none";
 };
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
-    VantaDark();
-} else {
-    VantaLight();
-}
+const NotSmart = function () {
+    document.querySelector('#JS-Background').style.display = "none";
+    document.querySelector('#JS-BackgroundDark').style.display = "none";
+};
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-    if (e.matches) {
+function VantaNoneSp() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
         VantaDark();
-
     } else {
         VantaLight();
     }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+        if (e.matches) {
+            VantaDark();
+
+        } else {
+            VantaLight();
+        }
+    });
+}
+
+
+
+if (window.matchMedia('(max-device-width: 530px)').matches == true) {
+    NotSmart();
+} else {
+    YesColor();
+}
+
+
+window.matchMedia('(max-device-width: 530px)').addEventListener('change', (e) => {
+    if (e.matches) {
+        NotSmart();
+
+    } else {
+        YesColor();
+    }
 });
-console.log("VantaDark");
