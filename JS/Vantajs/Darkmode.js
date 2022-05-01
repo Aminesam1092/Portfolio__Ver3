0f3a26bc413f5bb9c542a57b4fdc31d964d96a29
Aminesam1092 <doublaliamin13@gmail.com>
2022-05-01 15:07:50 +0900

    Recommit

--- JS/Vantajs/Darkmode.js
+++ JS/Vantajs/Darkmode.js
@@ -1,49 +1,51 @@
-const VantaDark = function () {
-    document.querySelector('#JS-Background').style.display = "none";
-    document.querySelector('#JS-BackgroundDark').style.display = "block";
-};
+document.addEventListener("DOMContentLoaded", function () {
+    const VantaDark = function () {
+        document.querySelector('#JS-Background').style.display = "none";
+        document.querySelector('#JS-BackgroundDark').style.display = "block";
+    };
 
-const VantaLight = function () {
-    document.querySelector('#JS-Background').style.display = "block";
-    document.querySelector('#JS-BackgroundDark').style.display = "none";
-};
+    const VantaLight = function () {
+        document.querySelector('#JS-Background').style.display = "block";
+        document.querySelector('#JS-BackgroundDark').style.display = "none";
+    };
 
-const NotSmart = function () {
-    document.querySelector('#JS-Background').style.display = "none";
-    document.querySelector('#JS-BackgroundDark').style.display = "none";
-};
+    const NotSmart = function () {
+        document.querySelector('#JS-Background').style.display = "none";
+        document.querySelector('#JS-BackgroundDark').style.display = "none";
+    };
 
-function VantaChangecolor() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
-        VantaDark();
-    } else {
-        VantaLight();
-    }
-
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-        if (e.matches) {
+    function VantaChangecolor() {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
             VantaDark();
-
         } else {
             VantaLight();
         }
-    });
-}
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+            if (e.matches) {
+                VantaDark();
+
+            } else {
+                VantaLight();
+            }
+        });
+    }
 
 
 
-if (window.matchMedia('(max-device-width: 530px)').matches == true) {
-    NotSmart();
-} else {
-    VantaChangecolor();
-}
-
-
-window.matchMedia('(max-device-width: 530px)').addEventListener('change', (e) => {
-    if (e.matches) {
+    if (window.matchMedia('(max-device-width: 530px)').matches == true) {
         NotSmart();
-
     } else {
         VantaChangecolor();
     }
+
+
+    window.matchMedia('(max-device-width: 530px)').addEventListener('change', (e) => {
+        if (e.matches) {
+            NotSmart();
+
+        } else {
+            VantaChangecolor();
+        }
+    });
 });
